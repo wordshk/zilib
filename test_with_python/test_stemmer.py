c130027eb09d52c94ceabe0c4baa49fbac780da3
WordsHk <info+github@words.hk>
2024-02-04 12:45:26 +0800

    Set up some unit tests

---
+++ test_with_python/test_stemmer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from pylib import latin as lib
+import zilib
+import unittest
+import bz2
+import csv
+
+
+class StemmerTest(unittest.TestCase):
+    def stem(self, word):
+        self.assertEqual(lib.american_english_stem(word), zilib.american_english_stem(word), f'Failed to stem {word}')
+
+    def test_degenerate(self):
+        self.stem('ies')
+
+    def test_simple(self):
+        self.stem('caresses')
+        self.stem('ponies')
+        self.stem('ties')
+        self.stem('caress')
+        self.stem('cats')
+        self.stem('feed')
+        self.stem('agreed')
+        self.stem('plastered')
+        self.stem('bled')
+        self.stem('motoring')
+        self.stem('sing')
+        self.stem('conflated')
+        self.stem('troubled')
+        self.stem('sized')
+        self.stem('hopping')
+        self.stem('tanned')
+        self.stem('falling')
+        self.stem('hissing')
+        self.stem('fizzed')
+        self.stem('failing')
+        self.stem('filing')
+        self.stem('happy')
+        self.stem('sky')
+        self.stem('quickly')
+        self.stem('running')
+        self.stem('dying')
+        self.stem('tying')
+        self.stem('flying')
+
+    def test_comprehensive(self):
+        # Open ../lists/en_unigram_freq.csv.bz2 and test all words
+        with bz2.open('../lists/en_unigram_freq.csv.bz2', 'rt') as f:
+            csvreader = csv.reader(f)
+            for row in csvreader:
+                self.stem(row[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
