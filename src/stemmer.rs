226fc588f7002d1000ec3d4d391abc93d0212e74
WordsHk <info+github@words.hk>
2024-02-04 13:59:14 +0800

    Small optimizations for the stemmer and add back comments about its origins

--- src/stemmer.rs
+++ src/stemmer.rs
@@ -1,7 +1,42 @@
 use pyo3::pyfunction;
 
+/* Ported from the python implementation in the links below. Code may look suspiciously like
+ * another rust implementation (https://github.com/minhnhdo/rust-stem/blob/master/src/lib.rs) due
+ * to convergence. I did reference the code there cross check whether my implementation was sane.
+ * Original comments are kept for reference:
+    """Porter Stemming Algorithm
+    This is the Porter stemming algorithm, ported to Python from the
+    version coded up in ANSI C by the author. It may be be regarded
+    as canonical, in that it follows the algorithm presented in
+
+    Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
+    no. 3, pp 130-137,
+
+    only differing from it at the points maked --DEPARTURE-- below.
+
+    See also http://www.tartarus.org/~martin/PorterStemmer
+
+    The algorithm as described in the paper could be exactly replicated
+    by adjusting the points of DEPARTURE, but this is barely necessary,
+    because (a) the points of DEPARTURE are definitely improvements, and
+    (b) no encoding of the Porter stemmer I have seen is anything like
+    as exact as this version, even with the points of DEPARTURE!
+
+    Vivake Gupta (v@nano.com)
+
+    Release 1: January 2001
+
+    Further adjustments by Santiago Bruno (bananabruno@gmail.com)
+    to allow word input not restricted to one word per line, leading
+    to:
+
+    release 2: July 2008
+    """
+*/
+
+/// Implementation of the Porter Stemming Algorithm in rust
 pub struct PorterStemmer {
-    b: String, // buffer for word to be stemmed
+    b: Vec<u8>, // Use vector of u8 instead of String since English words are ASCII
     k: usize,
     j: usize, // j is a general offset into the string
 }
@@ -16,9 +51,10 @@ impl PorterStemmer {
     Note that only lower case sequences are stemmed. Forcing to lower case
     should be done before stem(...) is called.
     "*/
-    fn new() -> Self {
+    fn new(word: &str) -> Self {
         Self {
-            b: String::new(),
+            // lower case ASCII a-z only, map from chars to u8
+            b: word.chars().filter(|c| c.is_ascii_alphabetic()).map(|c| c.to_ascii_lowercase() as u8).collect::<Vec<u8>>(),
             k: 0,
             j: 0,
         }
@@ -26,9 +62,9 @@ impl PorterStemmer {
 
     fn cons(&self, i: usize) -> bool {
         // cons(i) is TRUE <=> b[i] is a consonant.
-        match self.b.chars().nth(i).unwrap() { // FIXME: unwrap
-            'a' | 'e' | 'i' | 'o' | 'u' => false,
-            'y' => {
+        match self.b[i] {
+            b'a' | b'e' | b'i' | b'o' | b'u' => false,
+            b'y' => {
                 if i == 0 {
                     true
                 } else {
@@ -39,7 +75,7 @@ impl PorterStemmer {
         }
     }
 
-    fn m(&self) -> i32 {
+    fn m(&self) -> usize {
         /*"m() measures the number of consonant sequences between k0 and j.
         if c is a consonant sequence and v a vowel sequence, and <..>
         indicates arbitrary presence,
@@ -50,7 +86,7 @@ impl PorterStemmer {
            <c>vcvcvc<v> gives 3
            ....
         "*/
-        let mut n: i32 = 0; // FIXME: need i32?
+        let mut n: usize = 0;
         let mut i = 0;
 
         loop {
@@ -102,7 +138,7 @@ impl PorterStemmer {
         // doublec(j) is TRUE <=> j,(j-1) contain a double consonant.
         if j < 1 {
             false
-        } else if self.b.chars().nth(j) != self.b.chars().nth(j - 1) {
+        } else if self.b[j] != self.b[j - 1] {
             false
         } else {
             self.cons(j)
@@ -120,8 +156,8 @@ impl PorterStemmer {
         if i < 2 || !self.cons(i) || self.cons(i - 1) || !self.cons(i - 2) {
             false
         } else {
-            match self.b.chars().nth(i).unwrap() { // FIXME: unwrap
-                'w' | 'x' | 'y' => false,
+            match self.b[i] {
+                b'w' | b'x' | b'y' => false,
                 _ => true,
             }
         }
@@ -129,11 +165,12 @@ impl PorterStemmer {
 
     fn ends(&mut self, s: &str) -> bool {
         // ends(s) is TRUE <=> k0,...k ends with the string s.
-        let length = s.len();
+        let _s = s.as_bytes();
+        let length = _s.len();
         if length > self.k {
             return false;
         }
-        if self.b[self.k - length..self.k] == *s {
+        if &self.b[self.k - length..self.k] == _s {
             self.j = self.k - length;
             true
         } else {
@@ -142,9 +179,13 @@ impl PorterStemmer {
     }
 
     fn setto(&mut self, s: &str) {
-        // setto(s) sets (j+1),...k to the characters in the string s, readjusting k.
-        let length = s.len();
-        self.b = self.b[..self.j].to_string() + s + &self.b[self.j + length..];
+        // setto(s) sets j,...k to the characters in the string s, readjusting k.
+        let _s = s.as_bytes();
+        let length = _s.len();
+        for i in 0..length {
+            self.b[self.j + i] = _s[i];
+        }
+
         self.k = self.j + length;
     }
 
@@ -177,12 +218,12 @@ impl PorterStemmer {
            meetings  ->  meet
         "*/
 
-        if self.b.chars().nth(self.k - 1).unwrap() == 's' {
+        if self.b[self.k - 1] == b's' {
             if self.ends("sses") {
                 self.k -= 2;
             } else if self.ends("ies") {
                 self.setto("i");
-            } else if self.b.chars().nth(self.k - 2).unwrap() != 's' {
+            } else if self.b[self.k - 2] != b's' {
                 self.k -= 1;
             }
         }
@@ -200,8 +241,8 @@ impl PorterStemmer {
                 self.setto("ize");
             } else if self.doublec(self.k - 1) {
                 self.k -= 1;
-                let ch = self.b.chars().nth(self.k - 1).unwrap();
-                if ch == 'l' || ch == 's' || ch == 'z' {
+                let ch = self.b[self.k - 1];
+                if ch == b'l' || ch == b's' || ch == b'z' {
                     self.k += 1;
                 }
             } else if self.m() == 1 && self.cvc(self.k - 1) {
@@ -213,7 +254,7 @@ impl PorterStemmer {
     fn step1c(&mut self) {
         // step1c() turns terminal y to i when there is another vowel in the stem.
         if self.ends("y") && self.vowelinstem() {
-            self.b = self.b[..self.k - 1].to_string() + "i" + &self.b[self.k..];
+            self.b[self.k - 1] = b'i';
         }
     }
 
@@ -225,19 +266,19 @@ impl PorterStemmer {
         if self.k < 2 {
             return;
         }
-        match self.b.chars().nth(self.k - 2).unwrap() {
-            'a' => {
+        match self.b[self.k - 2] {
+            b'a' => {
                 if self.ends("ational") { self.r("ate"); }
                 else if self.ends("tional") { self.r("tion"); }
             },
-            'c' => {
+            b'c' => {
                 if self.ends("enci") { self.r("ence"); }
                 else if self.ends("anci") { self.r("ance"); }
             },
-            'e' => {
+            b'e' => {
                 if self.ends("izer") { self.r("ize"); }
             },
-            'l' => {
+            b'l' => {
                 if self.ends("bli") { self.r("ble"); } // DEPARTURE
                 // Original algorithm: if self.ends("abli") { self.r("able"); }
                 else if self.ends("alli") { self.r("al"); }
@@ -245,23 +286,23 @@ impl PorterStemmer {
                 else if self.ends("eli") { self.r("e"); }
                 else if self.ends("ousli") { self.r("ous"); }
             },
-            'o' => {
+            b'o' => {
                 if self.ends("ization") { self.r("ize"); }
                 else if self.ends("ation") { self.r("ate"); }
                 else if self.ends("ator") { self.r("ate"); }
             },
-            's' => {
+            b's' => {
                 if self.ends("alism") { self.r("al"); }
                 else if self.ends("iveness") { self.r("ive"); }
                 else if self.ends("fulness") { self.r("ful"); }
                 else if self.ends("ousness") { self.r("ous"); }
             },
-            't' => {
+            b't' => {
                 if self.ends("aliti") { self.r("al"); }
                 else if self.ends("iviti") { self.r("ive"); }
                 else if self.ends("biliti") { self.r("ble"); }
             },
-            'g' => {
+            b'g' => {
                 if self.ends("logi") { self.r("log"); } // DEPARTURE
                 // Original algorithm does not include this line
             },
@@ -272,20 +313,20 @@ impl PorterStemmer {
     fn step3(&mut self) {
         // step3() dels with -ic-, -full, -ness etc. similar strategy to step2.
         assert!(self.k > 0);
-        match self.b.chars().nth(self.k - 1).unwrap() {
-            'e' => {
+        match self.b[self.k - 1] {
+            b'e' => {
                 if self.ends("icate") { self.r("ic"); }
                 else if self.ends("ative") { self.r(""); }
                 else if self.ends("alize") { self.r("al"); }
             },
-            'i' => {
+            b'i' => {
                 if self.ends("iciti") { self.r("ic"); }
             },
-            'l' => {
+            b'l' => {
                 if self.ends("ical") { self.r("ic"); }
                 else if self.ends("ful") { self.r(""); }
             },
-            's' => {
+            b's' => {
                 if self.ends("ness") { self.r(""); }
             },
             _ => (),
@@ -297,60 +338,60 @@ impl PorterStemmer {
         if self.k < 2 {
             return;
         }
-        match self.b.chars().nth(self.k - 2).unwrap() {
-            'a' => {
+        match self.b[self.k - 2] {
+            b'a' => {
                 if self.ends("al") { }
                 else { return; }
             },
-            'c' => {
+            b'c' => {
                 if self.ends("ance") { }
                 else if self.ends("ence") { }
                 else { return; }
             },
-            'e' => {
+            b'e' => {
                 if self.ends("er") { }
                 else { return; }
             },
-            'i' => {
+            b'i' => {
                 if self.ends("ic") { }
                 else { return; }
             },
-            'l' => {
+            b'l' => {
                 if self.ends("able") { }
                 else if self.ends("ible") { }
                 else { return; }
             },
-            'n' => {
+            b'n' => {
                 if self.ends("ant") { }
                 else if self.ends("ement") { }
                 else if self.ends("ment") { }
                 else if self.ends("ent") { }
                 else { return; }
             },
-            'o' => {
-                if self.ends("ion") && (self.j > 1 && (self.b.chars().nth(self.j - 1).unwrap() == 's' || self.b.chars().nth(self.j - 1).unwrap() == 't')) { }
+            b'o' => {
+                if self.ends("ion") && (self.j > 1 && (self.b[self.j - 1] == b's' || self.b[self.j - 1] == b't')) { }
                 else if self.ends("ou") { }
                 // takes care of -ous
                 else { return; }
             },
-            's' => {
+            b's' => {
                 if self.ends("ism") { }
                 else { return; }
             },
-            't' => {
+            b't' => {
                 if self.ends("ate") { }
                 else if self.ends("iti") { }
                 else { return; }
             },
-            'u' => {
+            b'u' => {
                 if self.ends("ous") { }
                 else { return; }
             },
-            'v' => {
+            b'v' => {
                 if self.ends("ive") { }
                 else { return; }
             },
-            'z' => {
+            b'z' => {
                 if self.ends("ize") { }
                 else { return; }
             },
@@ -364,18 +405,19 @@ impl PorterStemmer {
     fn step5(&mut self) {
         //step5() removes a final -e if m() > 1, and changes -ll to -l if m() > 1.
         self.j = self.k;
-        if self.b.chars().nth(self.k - 1).unwrap() == 'e' {
+        if self.b[self.k - 1] == b'e' {
             let a = self.m();
             if a > 1 || (a == 1 && !self.cvc(self.k - 2)) {
                 self.k -= 1;
             }
         }
-        if self.b.chars().nth(self.k - 1).unwrap() == 'l' && self.doublec(self.k - 1) && self.m() > 1 {
+        if self.b[self.k - 1] == b'l' && self.doublec(self.k - 1) && self.m() > 1 {
             self.k -= 1;
         }
     }
 
-    fn stem(&mut self, p: &str) -> String {
+    fn stem(&mut self) -> String {
+        // Original comments below. We removed i, j, and moved p to new().
         /* In stem(p,i,j), p is a char pointer, and the string to be stemmed
         is from p[i] to p[j] inclusive. Typically i is zero and j is the
         offset to the last character of a string, (p[j+1] == '\0'). The
@@ -384,31 +426,28 @@ impl PorterStemmer {
         i <= k <= j. To turn the stemmer into a module, declare 'stem' as
         extern, and delete the remainder of this file.
         "*/
-        self.b = p.to_string();
         self.k = self.b.len();
 
         // With this line, strings of length 1 or 2 don't go through the
         // stemming process, although no mention is made of this in the
         // published algorithm. Remove the line to match the published
         // algorithm.
-        if self.k <= 2 {
-            return self.b.clone(); // --DEPARTURE--
+        if self.k > 2 {
+            self.step1ab();
+            self.step1c();
+            self.step2();
+            self.step3();
+            self.step4();
+            self.step5();
         }
 
-        self.step1ab();
-        self.step1c();
-        self.step2();
-        self.step3();
-        self.step4();
-        self.step5();
-        self.b[..self.k].to_string()
+        // unwrap is safe -- we know this will succeed due to the is_ascii_alphabetic check
+        String::from_utf8(self.b[..self.k].to_vec()).unwrap()
     }
 }
 
 #[pyfunction]
 pub fn american_english_stem(w: &str) -> String {
-    // lower case ASCII a-z only
-    let word = w.chars().filter(|c| c.is_ascii_alphabetic()).collect::<String>().to_lowercase();
-    let mut stemmer = PorterStemmer::new();
-    stemmer.stem(&word)
+    let mut stemmer = PorterStemmer::new(w);
+    stemmer.stem()
 }
